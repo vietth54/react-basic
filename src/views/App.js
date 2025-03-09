a13d733d9445647b7ce82d59b3798d00c1d42e8b
Vincent Trinh <thviet613@gmail.com>
2025-03-09 15:33:30 +0700

    hoc video #4

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.scss";
+import MyComponent from "./Example/MyComponent";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyComponent></MyComponent>
       </header>
     </div>
   );
---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  render() {
+    return <div>Hello from my Component</div>;
+  }
+}
+export default MyComponent;
